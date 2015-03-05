93eff56474043183f396c0912192befbce837685
Carlos Queiroz <cqueiroz@pivotal.io>
2015-03-05 14:38:53 +0800

    changed version to HK roadshow.

--- src/main/java/org/example/cities/VersionController.java
+++ src/main/java/org/example/cities/VersionController.java
@@ -8,6 +8,6 @@ public class VersionController {
 
     @RequestMapping("/version")
     public String index() {
-        return "Greetings Paris Roadshow!";
+        return "Greetings HK Roadshow!";
     }
 }
