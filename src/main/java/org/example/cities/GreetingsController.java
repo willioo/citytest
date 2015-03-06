641feb8e4f9efaea75578b1cb0e372edf109c5fe
Carlos Queiroz <cqueiroz@pivotal.io>
2015-03-06 16:04:40 +0800

    added Rest annotation.

--- src/main/java/org/example/cities/GreetingsController.java
+++ src/main/java/org/example/cities/GreetingsController.java
@@ -1,10 +1,12 @@
 package org.example.cities;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by cq on 6/3/15.
  */
+@RestController
 public class GreetingsController {
 
     @RequestMapping("/greetings")
