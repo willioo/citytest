5c78d48fea951119972f64d1dfd577814d333048
Carlos Queiroz <cqueiroz@pivotal.io>
2015-03-09 01:27:35 +0800

    replace /greetings for / endpoint.

--- src/main/java/org/example/cities/GreetingsController.java
+++ src/main/java/org/example/cities/GreetingsController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingsController {
 
-    @RequestMapping("/greetings")
+    @RequestMapping("/")
     public String index() {
         return "Greetings HK Roadshow!";
     }
